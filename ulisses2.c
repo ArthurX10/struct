c5db67fd919658533c7137538dbdf4b072b12e72
João Arthur <153871082+ArthurX10@users.noreply.github.com>
2025-02-03 19:44:21 -0300

    Create ulisses2.c

---
+++ ulisses2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Pessoa{
+    char nome[50];
+    int idade; 
+    char endereco[50];
+};
+
+
+int main(){
+    struct Pessoa pessoa1; 
+    
+    printf("Digite seu nome: ");
+    scanf(" %s", pessoa1.nome);
+    printf("Digite sua idade: ");
+    scanf("%d", &pessoa1.idade);
+    printf("Digite seu endereco: ");
+    scanf(" %s", pessoa1.endereco);
+    
+    
+     printf("\nDados da Pessoa:\n");
+    printf("Nome: %s\n", pessoa1.nome);
+    printf("Idade: %d\n", pessoa1.idade);
+    printf("Endereco: %s\n", pessoa1.endereco);
+}
