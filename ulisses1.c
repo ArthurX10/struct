ce0f45ca0d50dab3d42df9faf02377b6211b3933
João Arthur <153871082+ArthurX10@users.noreply.github.com>
2025-02-03 19:37:46 -0300

    Create ulisses1.c

---
+++ ulisses1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct  Horario
+{
+    int hora, minutos, segundos;
+    char compromisso[50];
+};
+
+int main(){
+    struct Horario horario;
+
+     printf("Informe o horario: (HH/MM/SS): ");
+     scanf("%d/%d/%d", &horario.hora, &horario.minutos, &horario. segundos);
+     printf("Informe o compromisso: ");
+     scanf(" %s", horario.compromisso);   
+     printf("Horario informado: %02d:%02d:%02d para o %s\n", horario.hora, horario.minutos, horario.segundos, horario.compromisso);
+}
